66dd39e17b9e71bf2feb9e2be83568d788e2e6b4
Chakradhara CS <149380548+chommulal@users.noreply.github.com>
2024-11-25 12:35:14 +0530

    Update test.sv

--- orange/b/test.sv
+++ orange/b/test.sv
@@ -1,31 +1,13 @@
-module job_q_2_1_tb;
-    reg InputA, InputB, InputC, InputD;  
-    wire OutputY;                        
-    
-    job_q_2_1 dut (
-        InputA, 
-        InputB, 
-        InputC, 
-        InputD, 
-        OutputY
-    );
-    initial begin
-        #20; InputA = 0; InputB = 0; InputC = 0; InputD = 0;
-        #10; InputA = 0; InputB = 0; InputC = 0; InputD = 1;
-        #10; InputA = 0; InputB = 0; InputC = 1; InputD = 0;
-        #10; InputA = 0; InputB = 0; InputC = 1; InputD = 1;
-        #10; InputA = 0; InputB = 1; InputC = 0; InputD = 0;
-        #10; InputA = 0; InputB = 1; InputC = 0; InputD = 1;
-        #10; InputA = 0; InputB = 1; InputC = 1; InputD = 0;
-        #10; InputA = 0; InputB = 1; InputC = 1; InputD = 1;
-        #10; InputA = 1; InputB = 0; InputC = 0; InputD = 0;
-        #10; InputA = 1; InputB = 0; InputC = 0; InputD = 1;
-        #10; InputA = 1; InputB = 0; InputC = 1; InputD = 0;
-        #10; InputA = 1; InputB = 0; InputC = 1; InputD = 1;
-        #10; InputA = 1; InputB = 1; InputC = 0; InputD = 0;
-        #10; InputA = 1; InputB = 1; InputC = 0; InputD = 1;
-        #10; InputA = 1; InputB = 1; InputC = 1; InputD = 0;
-        #10; InputA = 1; InputB = 1; InputC = 1; InputD = 1;
-    end
+module job_q_2.2_tb;
+reg A,B,C,D,E;
+wire Y;
+job_q_2.2  dut(A,B,C,D,E,Y);
+initial 
+begin
+#10;A=0;B=0;C=0;D=0;E=0;
+#10;A=0;B=0;C=0;D=0;E=1;
+#10;A=0;B=0;C=0;D=1;E=0;
+#10;A=0;B=0;C=0;D=1;E=1;
+end
 endmodule
 
